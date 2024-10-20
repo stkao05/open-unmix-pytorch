96e8e4d8108177e7a5be0c6fefba0e5a7d4d74df
Steven Kao <st.kao.05@gmail.com>
2024-10-20 08:57:02 +0800

    sdr

--- openunmix/sdr.py
+++ openunmix/sdr.py
@@ -2,22 +2,17 @@ import musdb
 from pathlib import Path
 import torch
 from openunmix import utils
-from openunmix import data
 from openunmix import predict
-from pathlib import Path
-import torch
 import torchaudio
-import json
-import numpy as np
 import tqdm
-import argparse
 import museval
-import mir_eval
+import os
 
-if __name__ == "__main__":
-    outdir = "separate-out"
-    targets = "vocals"
-    model_path = ""
+
+def run_eval():
+    model = "150"
+    outdir = f"separate-griffin/{model}"
+    model_path = f"open-unmix/{model}"
     sample_rate = 44100.0
 
     use_cuda = torch.cuda.is_available()
@@ -32,42 +27,51 @@ if __name__ == "__main__":
         is_wav=True,
         subsets="test",
     )
-    track = mus.tracks[0]
+    
+    for track in tqdm.tqdm(mus):
+        if os.path.exists(outdir_path / "test" / f"{track.name}.json"):
+            continue
+        
+        separator = utils.load_separator(
+            model_str_or_path=model_path,
+            targets=["vocals"],
+            niter=1,
+            wiener_win_len=300,
+            device=device,
+            pretrained=True,
+            residual=True,
+            filterbank="torch",
+        )
+        separator.freeze()
+        separator.to(device)
 
-    separator = utils.load_separator(
-        # model_str_or_path="open-unmix",
-        targets=["vocals"],
-        niter=1,
-        wiener_win_len=300,
-        device=device,
-        pretrained=True,
-        residual=True,
-        filterbank="torch",
-    )
-    separator.freeze()
-    separator.to(device)
-
-    estimates = predict.separate(
-        audio=torch.as_tensor(track.audio.T, dtype=torch.float32),
-        rate=sample_rate,
-        aggregate_dict=None,
-        separator=separator,
-        device=device,
-    )
-
-    Path(outdir_path / "test").mkdir(exist_ok=True, parents=True)
-    for target, estimate in estimates.items():
-        if target != "vocals": continue
-        target_path = str(outdir_path / "test" / Path(track.name + "-" + target).with_suffix(".wav"))
-        torchaudio.save(
-            target_path,
-            torch.squeeze(estimate).to("cpu"),
-            sample_rate=separator.sample_rate,
+        estimates = predict.separate(
+            audio=torch.as_tensor(track.audio.T, dtype=torch.float32),
+            rate=sample_rate,
+            aggregate_dict=None,
+            separator=separator,
+            device=device,
+            griffin=True
         )
 
-    eval_est = {
-        'vocals': estimates["vocals"].squeeze().T.cpu().numpy(),
-        'accompaniment': estimates["residual"].squeeze().T.cpu().numpy(),
-    }
-    scores = museval.eval_mus_track(track, eval_est, output_dir=outdir_path)
-    print(scores)
+        Path(outdir_path / "test").mkdir(exist_ok=True, parents=True)
+        
+        for target, estimate in estimates.items():
+            if target != "vocals": continue
+            target_path = str(outdir_path / "test" / Path(track.name + "-" + target).with_suffix(".wav"))
+            torchaudio.save(
+                target_path,
+                torch.squeeze(estimate).to("cpu"),
+                sample_rate=separator.sample_rate,
+            )
+
+        eval_est = {
+            'vocals': estimates["vocals"].squeeze().T.cpu().numpy(),
+            'accompaniment': estimates["residual"].squeeze().T.cpu().numpy(),
+        }
+
+        museval.eval_mus_track(track, eval_est, output_dir=outdir_path)
+
+        
+if __name__ == "__main__":
+    run_eval()
